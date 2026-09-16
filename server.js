df1adba01bc98f9af599fc9810efc3db2aff04e1
Olivia Igbinosa <oi506332@gmail.com>
2026-09-16 02:23:59 +0100

    testing

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(helmet())
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://voucher-system-client.netlify.app',
+  origin: 'http://localhost:3000' || 'https://voucher-system-client.netlify.app',
   credentials: true
 }))
 
